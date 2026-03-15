a9e1addb62f767740d2875c85571492a767ffa5e
Amritpal Bhinder <bhinderrajwinderkaur407@gmail.com>
2026-03-15 22:57:36 +0530

    Refactor array display code for simplicity

--- DSA/Array/Display.js
+++ DSA/Array/Display.js
@@ -1,7 +1,5 @@
-// Wap to create an array and display the elements of array.
+// Wap to create an array and display its elements.
 
-const arr= [1, 2, 3, 4, 5]
+let arr = [1,2,3,4,5]
 
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
+console.log(arr)    
