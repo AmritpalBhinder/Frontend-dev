01eb35d59bc7b0124ce3ca1cdb21c4d958764d78
Amritpal Bhinder <bhinderrajwinderkaur407@gmail.com>
2026-03-18 23:20:28 +0530

    Update Count.js

--- DSA/Array/Count.js
+++ DSA/Array/Count.js
@@ -5,13 +5,10 @@ let arr = [10,52,36,52,85]
 
 for(let i=0;i<arr.length;i++){
     let count = 1;
-    for(let j=i+1;i<arr.length;j++){
+    for(let j=i+1;j<arr.length;j++){
         if(arr[i]==arr[j]){
             count++;
-           
         }
     }
-
+    console.log(arr[i] + " occurs " + count + " times");
 }
-
-console.log("Count of occurencies of an element in an array is: "+count);
